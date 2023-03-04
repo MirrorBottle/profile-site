e1c588790f9adc95e99da398f2c11a2a57a124e8
MirrorBottle <setiawanbayu66152@gmail.com>
2023-03-04 17:48:06 +0800

    feat: update new project

--- src/mock/data.js
+++ src/mock/data.js
@@ -52,7 +52,11 @@ export const iconsData = {
   </svg>`,
   node: `<svg viewBox="0 0 128 128" style="height: 20px; margin-right: 10px;">
   <path fill="#83CD29" d="M112.771 30.334L68.674 4.729c-2.781-1.584-6.402-1.584-9.205 0L14.901 30.334C12.031 31.985 10 35.088 10 38.407v51.142c0 3.319 2.084 6.423 4.954 8.083l11.775 6.688c5.628 2.772 7.617 2.772 10.178 2.772 8.333 0 13.093-5.039 13.093-13.828v-50.49c0-.713-.371-1.774-1.071-1.774h-5.623C42.594 41 41 42.061 41 42.773v50.49c0 3.896-3.524 7.773-10.11 4.48L18.723 90.73c-.424-.23-.723-.693-.723-1.181V38.407c0-.482.555-.966.982-1.213l44.424-25.561c.415-.235 1.025-.235 1.439 0l43.882 25.555c.42.253.272.722.272 1.219v51.142c0 .488.183.963-.232 1.198l-44.086 25.576c-.378.227-.847.227-1.261 0l-11.307-6.749c-.341-.198-.746-.269-1.073-.086-3.146 1.783-3.726 2.02-6.677 3.043-.726.253-1.797.692.41 1.929l14.798 8.754a9.294 9.294 0 004.647 1.246c1.642 0 3.25-.426 4.667-1.246l43.885-25.582c2.87-1.672 4.23-4.764 4.23-8.083V38.407c0-3.319-1.36-6.414-4.229-8.073zM77.91 81.445c-11.726 0-14.309-3.235-15.17-9.066-.1-.628-.633-1.379-1.272-1.379h-5.731c-.709 0-1.279.86-1.279 1.566 0 7.466 4.059 16.512 23.453 16.512 14.039 0 22.088-5.455 22.088-15.109 0-9.572-6.467-12.084-20.082-13.886-13.762-1.819-15.16-2.738-15.16-5.962 0-2.658 1.184-6.203 11.374-6.203 9.105 0 12.461 1.954 13.842 8.091.118.577.645.991 1.24.991h5.754c.354 0 .692-.143.94-.396.24-.272.367-.613.335-.979-.891-10.568-7.912-15.493-22.112-15.493-12.631 0-20.166 5.334-20.166 14.275 0 9.698 7.497 12.378 19.622 13.577 14.505 1.422 15.633 3.542 15.633 6.395 0 4.955-3.978 7.066-13.309 7.066z"></path>
-  </svg>`
+  </svg>`,
+  flutter: `<svg viewBox="0 0 128 128" style="height: 20px; margin-right: 5px;">
+  <g fill="#3FB6D3"><path d="M12.3 64.2L76.3 0h39.4L32.1 83.6zM76.3 128h39.4L81.6 93.9l34.1-34.8H76.3L42.2 93.5z"></path></g><path fill="#27AACD" d="M81.6 93.9l-20-20-19.4 19.6 19.4 19.6z"></path><path fill="#19599A" d="M115.7 128L81.6 93.9l-20 19.2L76.3 128z"></path><linearGradient id="flutter-original-a" gradientUnits="userSpaceOnUse" x1="59.365" y1="116.36" x2="86.825" y2="99.399"><stop offset="0" stop-color="#1b4e94"></stop><stop offset=".63" stop-color="#1a5497"></stop><stop offset="1" stop-color="#195a9b"></stop></linearGradient><path fill="url(#flutter-original-a)" d="M61.6 113.1l30.8-8.4-10.8-10.8z"></path>
+  </svg>
+`
 };
 
 // PROJECTS DATA
@@ -198,7 +202,7 @@ export const projectsData = [
     id: nanoid(),
     img: 'anandita.png',
     title: 'PT. Anandita Profile',
-    client: 'PT. Anandita Putra Akmal',
+    client: 'PT. Anandita Putri Akmal',
     techs: [
       { name: "laravel", title: "Laravel 8" },
       { name: "jquery", title: "Jquery" },
@@ -256,6 +260,39 @@ export const projectsData = [
       { name: "node", title: "Node JS" },
       { name: "vue", title: "Vue 2" },
     ],
+  },
+  {
+    id: nanoid(),
+    img: 'pt-anandita-app.png',
+    title: 'PT. Anandita App',
+    client: 'PT. Anandita Putri Akmal',
+    techs: [
+      { name: "flutter", title: "Flutter 2" },
+      { name: "laravel", title: "Laravel 8" }
+    ],
+  },
+  {
+    id: nanoid(),
+    img: 'citra-mobile.png',
+    title: 'Citra99 Mobile',
+    client: 'Citra99',
+    techs: [
+      { name: "flutter", title: "Flutter 2" },
+      { name: "laravel", title: "Laravel 7" }
+    ],
+    url: 'https://play.google.com/store/apps/details?id=com.citra99.citra99mobile'
+  },
+  {
+    id: nanoid(),
+    img: 'citra-profile.png',
+    title: 'Citra99 Company Profile & Auction',
+    client: 'Citra99',
+    techs: [
+      { name: "jquery", title: "Jquery" },
+      { name: "laravel", title: "Laravel 8" },
+      { name: "bootstrap", title: "Bootstrap 4" }
+    ],
+    url: 'https://citra99.com/'
   },
 ];
 
